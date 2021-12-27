329a5ac164bb1eefea8e424b936e176c3c114c50
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-12-27 11:29:00 +0000

    Added sessions to the API

--- GeodesignHub.py
+++ GeodesignHub.py
@@ -2,65 +2,67 @@ import requests, json
 
 class GeodesignHubClient():
 	'''
-	This a a Python client that uses the Geodesign Hub API to make calls
+	This a a Python client that make calls to the Geodesignhub API
 	and return data. It requires the requests package and the json module. 
 
 	'''
+
 	def __init__(self, url, token, project_id):
 		'''
 		Declare your project id, token and the url (optional). 
 		'''
 		self.projectid = project_id
 		self.token = token
-		self.securl = url if url else 'https://www.geodesignhub.com/api/v1/'
+		self.securl = url if url else 'https://www.geodesignhub.com/api/v1/'		
+		self.session = requests.Session()
 
 	def get_systems(self):
 		''' This method gets all systems for a particular project.  '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_diagrams(self):
 		''' This method gets a list of diagrams for a particular project. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'diagrams' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_constraints(self):
 		''' This method gets the geometry of constraints for a project if available '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'constraints' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_first_boundaries(self):
 		''' Gets the first boundaries if defined for a project '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'boundaries' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_second_boundaries(self):
 		''' Gets the second boundaries if defined for a project.  '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'secondboundaries' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_project_bounds(self):
 		''' Returns a string with bounding box for the project study area coordinates in a 'southwest_lng,southwest_lat,northeast_lng,northeast_lat' format. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'bounds' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_changeteams(self):
 		''' Return all the change teams for that project. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'cteams' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_changeteam(self, teamid):
@@ -68,28 +70,28 @@ class GeodesignHubClient():
 		assert type(teamid) is int, "Team id is not a integer: %r" % teamid
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'cteams' + '/'+ str(teamid) +'/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 		
 	def get_synthesis(self, teamid, synthesisid):
 		assert type(teamid) is int, "Team id is not a integer: %r" % teamid
 		securl = self.securl + 'projects'+ '/' + self.projectid + '/cteams/'+ str(teamid) +'/' + str(synthesisid) + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_synthesis_timeline(self, teamid, synthesisid):
 		assert type(teamid) is int, "Team id is not a integer: %r" % teamid
 		securl = self.securl + 'projects'+ '/' + self.projectid + '/cteams/'+ str(teamid) +'/' + str(synthesisid) + '/timeline/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_synthesis_diagrams(self, teamid, synthesisid):
 		assert type(teamid) is int, "Team id is not a integer: %r" % teamid
 		securl = self.securl + 'projects'+ '/' + self.projectid + '/cteams/'+ str(teamid) +'/' + str(synthesisid) + '/diagrams/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_changeteam_members(self, teamid):
@@ -97,7 +99,7 @@ class GeodesignHubClient():
 		assert type(teamid) is int, "Team id is not a integer: %r" % teamid
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'cteams' + '/'+ str(teamid) +'/' +'members' + '/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_synthesis_system_projects(self, sysid, teamid, synthesisid):
@@ -105,15 +107,15 @@ class GeodesignHubClient():
 		assert type(sysid) is int ,"System id is not a integer %r" % sysid
 		securl = self.securl + 'projects'+ '/' + self.projectid + '/cteams/'+ str(teamid) +'/' + str(synthesisid) + '/systems/' + str(sysid) + '/projects/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def post_as_diagram(self, geoms, projectorpolicy, featuretype, description, sysid, fundingtype):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems'+'/'+ str(sysid) + '/'+ 'add' +'/' + projectorpolicy +'/'
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
 		postdata = {'geometry':geoms, 'description':description, 'featuretype':featuretype, 'fundingtype':fundingtype}
-		r = requests.post(securl, headers= headers, data = json.dumps(postdata))
+		r = self.session.post(securl, headers= headers, data = json.dumps(postdata))
 		return r
 
 	def get_diagram(self, diagid):
@@ -121,7 +123,14 @@ class GeodesignHubClient():
 		assert type(diagid) is int, "diagram id is not an integer: %r" % id
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'diagrams' + '/'+ str(diagid) +'/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
+		return r
+
+	def get_all_diagrams(self):
+		''' This method gets the geometry of all diagrams in a project .  '''		
+		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' + 'diagrams/all/'
+		headers = {'Authorization': 'Token '+ self.token}
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def get_diagram_changeid(self, diagid):
@@ -129,52 +138,52 @@ class GeodesignHubClient():
 		assert type(diagid) is int, "diagram id is not an integer: %r" % id
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'diagrams' + '/'+ str(diagid) +'/changeid/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.get(securl, headers=headers)
+		r = self.session.get(securl, headers=headers)
 		return r
 
 	def post_as_ealuation_JSON(self, geoms, sysid, username=None):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems'+'/'+ str(sysid) + '/e/map/json/'
 		if username:
 			securl += username +'/'
 		
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
 
-		r = requests.post(securl, headers= headers, data = json.dumps(geoms))
+		r = self.session.post(securl, headers= headers, data = json.dumps(geoms))
 		return r
 
 	def post_as_impact_JSON(self, geoms, sysid, username=None):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems'+'/'+ str(sysid) + '/i/map/json/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
-		r = requests.post(securl, headers= headers, data = json.dumps(geoms))
+		r = self.session.post(securl, headers= headers, data = json.dumps(geoms))
 		return r
 
 	def post_as_evaluation_GBF(self, geoms, sysid, username=None):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems'+'/'+ str(sysid) + '/e/map/gbf/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geoms})
+		r = self.session.post(securl, headers= headers, files = {'geoms.gbf':geoms})
 		return r
 
 
 	def post_gdservice_JSON(self, geometry, jobid):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'gdservices/callback/'
 		headers = {'Authorization': 'Token '+ self.token, 'content-type': 'application/json'}
 		data = {"geometry": geometry, "jobid": jobid}
-		r = requests.post(securl, headers= headers, data = json.dumps(data))
+		r = self.session.post(securl, headers= headers, data = json.dumps(data))
 		return r
 
 	def post_as_impact_GBF(self, geoms, sysid, username=None):
-		''' Create a requests object with correct headers and creds. '''
+		''' Create a self.session object with correct headers and creds. '''
 		securl = self.securl+ 'projects'+ '/' + self.projectid + '/' +'systems'+'/'+ str(sysid) + '/i/map/gbf/'
 		if username:
 			securl += username +'/'
 		headers = {'Authorization': 'Token '+ self.token}
-		r = requests.post(securl, headers= headers, files = {'geoms.gbf':geoms})
+		r = self.session.post(securl, headers= headers, files = {'geoms.gbf':geoms})
 		return r
